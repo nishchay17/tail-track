1df6bbb47fa75713c8228f1724dd206ffe2e8486
Nishchay <nishchay13971@gmail.com>
2024-03-13 22:39:09 +0530

    build: fixes

--- app/providers.ts
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { SessionProvider } from "next-auth/react";
-
-function Providers({ children }: { children: React.ReactNode }) {
-  return <SessionProvider session={session}>{children}</SessionProvider>;
-}
-
-export default Providers;
