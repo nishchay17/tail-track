925c6f38715e3d6c2a6e097510da23604a09b3d8
Nishchay <nishchay13971@gmail.com>
2024-03-17 03:49:05 +0530

    feat: api table index

--- lib/db/schema/api-key.ts
+++ lib/db/schema/api-key.ts
@@ -1,15 +1,24 @@
-import { pgTable, text, timestamp } from "drizzle-orm/pg-core";
+import { index, pgTable, text, timestamp } from "drizzle-orm/pg-core";
 
 import { users } from ".";
 
-export const apiKey = pgTable("api-key", {
-  id: text("id").notNull().primaryKey(),
-  userId: text("userId")
-    .notNull()
-    .references(() => users.id, { onDelete: "cascade" }),
-  token: text("token").notNull(),
-  createdAt: timestamp("created_at").defaultNow().notNull(),
-});
+export const apiKey = pgTable(
+  "api-key",
+  {
+    id: text("id").notNull().primaryKey(),
+    userId: text("userId")
+      .notNull()
+      .references(() => users.id, { onDelete: "cascade" }),
+    token: text("token").notNull(),
+    createdAt: timestamp("created_at").defaultNow().notNull(),
+  },
+  (table) => {
+    return {
+      tokenIdx: index("token_idx").on(table.token),
+      userIdIdx: index("user_id_idx").on(table.userId),
+    };
+  }
+);
 
 export type APIKeyType = {
   id: string;
