57db1e1520cc86465d22a365923081c3a7bff1d2
Nishchay <nishchay13971@gmail.com>
2024-03-18 01:42:18 +0530

    feat: mobile landing

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ import { useToast } from "@/components/ui/use-toast";
 export default function Home() {
   const [_, copyToClipboard] = useCopyToClipboard();
   const { toast } = useToast();
-  const _code = `const res = await tailtrack({namespace: "portfolio"});`;
+  const _code = ``;
 
   return (
     <main>
@@ -25,7 +25,7 @@ export default function Home() {
           className="justify-between items-center my-6"
         />
       </div>
-      <section className="mt-[18vh] flex flex-col items-center justify-center">
+      <section className="mb-48 md:mb-0 mt-[18vh] flex flex-col items-center justify-center">
         <Button
           onClick={() => {
             copyToClipboard("npm install tail-track");
@@ -38,14 +38,14 @@ export default function Home() {
         >
           {">"} npm install tail-track
         </Button>
-        <h1 className="text-7xl text-center leading-[120%] font-semibold">
+        <h1 className="text-4xl md:text-6xl lg:text-7xl text-center leading-[120%] font-semibold">
           Track analytics with
           <br />
-          <span className="bg-gradient-to-br to-violet-600 from-violet-400 bg-clip-text text-transparent">
+          <span className="mt-2 bg-gradient-to-br to-violet-600 from-violet-400 bg-clip-text text-transparent">
             Tail Track
           </span>
         </h1>
-        <p className="mt-6 opacity-70 text-lg">
+        <p className="mt-6 opacity-70 text-sm md:text-base lg:text-lg text-center px-10">
           Tail track is a free and open-source analytics platform, for all your
           projects
         </p>
@@ -67,74 +67,87 @@ export default function Home() {
           </Link>
         </div>
       </section>
-      <div className="mt-48 mb-36 container max-w-[1000px] p-4 border border-white/20 rounded-lg">
-        <Image className=" mx-auto" src={dashboardImage} alt="dashboard" />
+      <div className="container max-w-[1000px]">
+        <div className="hidden sm:block mt-48 border border-white/20 rounded-lg">
+          <Image className=" mx-auto" src={dashboardImage} alt="dashboard" />
+        </div>
       </div>
-      <span id="setup" />
-      <section className="container max-w-[1000px] px-4 lg:px-0 mb-36 py-8 border border-white/20 rounded-lg">
-        <h2 className="text-4xl font-semibold text-center mb-2">Easy setup</h2>
-        <p className="text-center opacity-70 mb-8">
-          Setup Tail Track in just a few clicks and start tracking
-        </p>
-        <div className="mx-auto max-w-[500px] space-y-3 mb-5">
-          <div className="relative">
-            <div className="absolute top-[7px] left-[-20px] h-3 w-3 bg-violet-300 rounded-full" />
-            <div className="absolute top-[19px] left-[-15px] h-[100%] w-[2px] bg-violet-300" />
-            <p className="text-lg opacity-70">Step 1:</p>
-            <p>
-              Signup with Github{" "}
-              <span className="opacity-70">(other providers coming soon)</span>
-            </p>
-          </div>
-          <div className="relative">
-            <div className="absolute top-[7px] left-[-20px] h-3 w-3 bg-violet-300 rounded-full" />
-            <div className="absolute top-[19px] left-[-15px] h-[100%] w-[2px] bg-violet-300" />
-            <p className="text-lg opacity-70">Step 2:</p>
-            <p className="leading-[170%]">
-              Copy your API token{" "}
-              <span className="opacity-70">(from Integration tab)</span>, and
-              paste it in{" "}
-              <span className="bg-violet-500/20 px-[5px] py-[1px] rounded-md border border-white/10 leading-[0]">
-                .env
-              </span>{" "}
-              file with name{" "}
-              <span className="bg-violet-500/20 px-[5px] py-[1px] rounded-md border border-white/10 leading-[0]">
-                TAILTRACK_API_KEY
-              </span>
-            </p>
-          </div>
-          <div className="relative">
-            <div className="absolute top-[7px] left-[-20px] h-3 w-3 bg-violet-300 rounded-full" />
-            <div className="absolute top-[19px] left-[-15px] h-[100%] w-[2px] bg-violet-300" />
-            <p className="text-lg opacity-70">Step 3:</p>
-            <p>
-              Add our package from NPM to your project{" "}
-              <span className="bg-violet-500/20 px-[5px] py-[1px] rounded-md border border-white/10 leading-[0]">
-                tail-track
-              </span>
-            </p>
-          </div>
-          <div className="relative">
-            <div className="absolute top-[7px] left-[-20px] h-3 w-3 bg-violet-300 rounded-full" />
-            <p className="text-lg opacity-70">Step 4:</p>
-            <p className="mb-1">Pass namespace and options to start tracking</p>
-            <pre className="bg-violet-500/20 px-[5px] py-[1px] rounded-md border border-white/10 w-fit">
-              <code>{_code}</code>
-            </pre>
+      <section id="setup" className="container max-w-[1000px]">
+        <div className="max-w-[1000px] px-8 lg:px-0 my-20 md:my-36 py-8 border border-white/20 rounded-lg">
+          <h2 className="text-3xl lg:text-4xl font-semibold text-center mb-2">
+            Easy setup
+          </h2>
+          <p className="text-center opacity-70 mb-8">
+            Setup Tail Track in just a few clicks and start tracking
+          </p>
+          <div className="mx-auto max-w-[500px] space-y-3 mb-5">
+            <div className="relative">
+              <div className="absolute top-[7px] left-[-20px] h-3 w-3 bg-violet-300 rounded-full" />
+              <div className="absolute top-[19px] left-[-15px] h-[100%] w-[2px] bg-violet-300" />
+              <p className="text-lg opacity-70">Step 1:</p>
+              <p>
+                Signup with Github{" "}
+                <span className="opacity-70">
+                  (other providers coming soon)
+                </span>
+              </p>
+            </div>
+            <div className="relative">
+              <div className="absolute top-[7px] left-[-20px] h-3 w-3 bg-violet-300 rounded-full" />
+              <div className="absolute top-[19px] left-[-15px] h-[100%] w-[2px] bg-violet-300" />
+              <p className="text-lg opacity-70">Step 2:</p>
+              <p className="leading-[170%]">
+                Copy your API token{" "}
+                <span className="opacity-70">(from Integration tab)</span>, and
+                paste it in{" "}
+                <span className="bg-violet-500/20 px-[5px] py-[1px] rounded-md border border-white/10 leading-[0]">
+                  .env
+                </span>{" "}
+                file with name{" "}
+                <span className="bg-violet-500/20 px-[5px] py-[1px] rounded-md border border-white/10 leading-[0]">
+                  TAILTRACK_API_KEY
+                </span>
+              </p>
+            </div>
+            <div className="relative">
+              <div className="absolute top-[7px] left-[-20px] h-3 w-3 bg-violet-300 rounded-full" />
+              <div className="absolute top-[19px] left-[-15px] h-[100%] w-[2px] bg-violet-300" />
+              <p className="text-lg opacity-70">Step 3:</p>
+              <p>
+                Add our package from NPM to your project{" "}
+                <span className="bg-violet-500/20 px-[5px] py-[1px] rounded-md border border-white/10 leading-[0]">
+                  tail-track
+                </span>
+              </p>
+            </div>
+            <div className="relative">
+              <div className="absolute top-[7px] left-[-20px] h-3 w-3 bg-violet-300 rounded-full" />
+              <p className="text-lg opacity-70">Step 4:</p>
+              <p className="mb-1">
+                Pass namespace and options to start tracking
+              </p>
+              <pre className="bg-violet-500/20 px-[5px] py-[1px] rounded-md border border-white/10 w-full md:w-fit overflow-auto">
+                <code>
+                  const res = await tailtrack{"({"}namespace: {'"portfolio"});'}
+                </code>
+              </pre>
+            </div>
           </div>
         </div>
       </section>
-      <section className="text-center py-24 border-t border-b border-white/20">
-        <h2 className="text-4xl font-semibold mb-5">
+      <section className="text-center py-14 md:py-24 border-t border-b border-white/20">
+        <h2 className="text-3xl lg:text-4xl font-semibold mb-3 md:mb-5">
           Let{"'"}s get{" "}
           <span className="bg-gradient-to-br to-violet-600 from-violet-400 bg-clip-text text-transparent">
             started
           </span>
         </h2>
-        <p className="mb-10 opacity-70 text-lg">Start using Tail Track today</p>
+        <p className="mb-7 md:mb-10 opacity-70 text-lg">
+          Start using Tail Track today
+        </p>
         <Button size="lg">Start for free</Button>
       </section>
-      <footer className="pt-24 pb-10 text-center">
+      <footer className="pt-10 md:pt-24 pb-10 text-center">
         <p className="mb-1">
           Created by{" "}
           <Link
--- components/nav/main-nav.tsx
+++ components/nav/main-nav.tsx
@@ -23,20 +23,20 @@ export function MainNav({
 
   return (
     <div className={cn("flex gap-6 md:gap-10", className)}>
-      <Link href="#" className="hidden items-center space-x-2 md:flex">
+      <Link href="#" className="items-center space-x-2 flex">
         <Icons.logo className="h-5 w-5" />
         <span className="hidden text-lg font-semibold sm:inline-block">
           {siteConfig.name}
         </span>
       </Link>
       {items?.length ? (
-        <nav className="hidden gap-6 md:flex">
+        <nav className="gap-6 flex">
           {items?.map((item, index) => (
             <Link
               key={index}
               href={item.disabled ? "#" : item.href}
               className={cn(
-                "flex items-center text-lg font-medium transition-colors hover:text-foreground/80 sm:text-sm",
+                "flex items-center text-base md:text-lg font-medium transition-colors hover:text-foreground/80 sm:text-sm",
                 item.href.startsWith(`/${segment}`)
                   ? "text-foreground"
                   : "text-foreground/60",
