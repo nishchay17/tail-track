a25c198a1907c05f1fc250d8468440584328cba2
Nishchay <nishchay13971@gmail.com>
2024-03-19 23:29:11 +0530

    feat: rate limiter

---
+++ lib/rate-limiter.ts
@@ -0,0 +1,7 @@
+import { Ratelimit } from "@upstash/ratelimit";
+import {redis} from "./redis";
+
+export const rateLimiter = new Ratelimit({
+  redis,
+  limiter: Ratelimit.slidingWindow(5, "1 s")
+});
--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,32 @@
 import { NextRequest, NextResponse } from "next/server";
 import { analytics } from "@/util/analytics";
+import { rateLimiter } from "./lib/rate-limiter";
 
 export default async function middleware(req: NextRequest) {
+  const ip = req.headers.get("x-forwarded-for");
+
+  if (req.nextUrl.pathname.startsWith("/api/v1")) {
+    const { success, limit, reset, remaining } = await rateLimiter.limit(
+      `ratelimit_${ip}`
+    );
+    if (!success) {
+      return new Response(
+        JSON.stringify({
+          error: true,
+          message: "rate limit exceeded, 5 request per second allowed",
+        }),
+        {
+          status: 429,
+          headers: {
+            "X-RateLimit-Limit": limit.toString(),
+            "X-RateLimit-Remaining": remaining.toString(),
+            "X-RateLimit-Reset": reset.toString(),
+          },
+        }
+      );
+    }
+  }
+
   if (req.nextUrl.pathname === "/") {
     analytics
       .track("59cbe83c-f547-48a9-b820-b6d7d743f94f", "pageview", {
@@ -13,4 +38,7 @@ export default async function middleware(req: NextRequest) {
 
   return NextResponse.next();
 }
-analytics.track;
+
+export const config = {
+  matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
+};
